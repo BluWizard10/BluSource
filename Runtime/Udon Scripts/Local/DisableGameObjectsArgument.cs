23a12a0a59ed332bcda420c0fe016f48405c7006
BluWizard <15589012-BluWizard10@users.noreply.gitlab.com>
2023-10-23 01:24:52 -0700

    Initial Commit.

---
+++ Runtime/Udon Scripts/Local/DisableGameObjectsArgument.cs
@@ -0,0 +1,28 @@
+﻿#if UDONSHARP
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+// Written by BluWizard - https://github.com/BluWizard10
+
+namespace BluWizard.Udon
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class DisableGameObjectsArgument : UdonSharpBehaviour
+    {
+        [Header("GameObjects to Disable")]
+        [Tooltip("List of GameObjects that should be Disabled when this script runs.")]
+        public GameObject[] toDisable;
+
+        public void _BluEvent()
+        {
+            foreach (GameObject disable in toDisable)
+            {
+                disable.SetActive(false);
+            }
+        }
+    }
+}
+#endif
